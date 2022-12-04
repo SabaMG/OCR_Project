dd11cb29449799c1d51040afe55affd1a36b566f
toutane <ca@leger.email>
2022-12-04 16:25:30 +0100

    add about dialog

--- final/main.c
+++ final/main.c
@@ -2,12 +2,12 @@
 
 // Structure of the graphical user interface.
 typedef struct UserInterface {
-	//GtkBuilder* builder;
     GtkWindow* window;
 	GtkHeaderBar* header_bar;
 	GtkImage* main_image;
 	GtkButton* resolve_button;
 	GtkDialog* nn_dialog;
+	GtkDialog* about_dialog;
 } UserInterface;
 
 gboolean on_open_button(GtkWidget *widget, gpointer user_data) {
@@ -50,17 +50,17 @@ gboolean on_open_button(GtkWidget *widget, gpointer user_data) {
 
 gboolean on_nn_menu_button(GtkWidget* width, gpointer user_data) {
 	UserInterface *ui = user_data;
-	//GtkDialog* nn_dialog = GTK_DIALOG(gtk_builder_get_object(ui->builder, "neural_network_dialog"));
 	gtk_widget_show(GTK_WIDGET(ui->nn_dialog));
-	//gint res = gtk_dialog_run (GTK_DIALOG(nn_dialog));
-	g_signal_connect_swapped (ui->nn_dialog,
-			"response",
-			//G_CALLBACK (gtk_widget_destroy),
-			G_CALLBACK(gtk_widget_hide),
-			ui->nn_dialog);
-	//g_print("%i\n", res);
+	g_signal_connect_swapped (ui->nn_dialog, "response", G_CALLBACK(gtk_widget_hide), ui->nn_dialog);
 	g_signal_connect_swapped(ui->nn_dialog, "delete-event", G_CALLBACK(gtk_widget_hide_on_delete), ui->nn_dialog);
-	//g_signal_connect(nn_dialog, "delete-event", G_CALLBACK(gtk_widget_hide_on_delete(GTK_WIDGET(nn_dialog))), NULL);
+	return FALSE;
+}
+
+gboolean on_about_menu_button(GtkWidget* width, gpointer user_data) {
+	UserInterface *ui = user_data;
+	gtk_widget_show(GTK_WIDGET(ui->about_dialog));
+	g_signal_connect_swapped (ui->about_dialog, "response", G_CALLBACK(gtk_widget_hide), ui->about_dialog);
+	g_signal_connect_swapped(ui->about_dialog, "delete-event", G_CALLBACK(gtk_widget_hide_on_delete), ui->about_dialog);
 	return FALSE;
 }
 
@@ -85,23 +85,24 @@ int main (int argc, char **argv) {
 	GtkButton* resolve_button = GTK_BUTTON(gtk_builder_get_object(builder, "resolve_button"));
 	GtkModelButton* nn_menu_button = GTK_MODEL_BUTTON(gtk_builder_get_object(builder, "neural_network_menu_button"));
 	GtkDialog* nn_dialog = GTK_DIALOG(gtk_builder_get_object(builder, "neural_network_dialog"));
-	//gtk_widget_set_parent(GTK_WIDGET(nn_dialog), GTK_WIDGET(window));
+	GtkModelButton* about_menu_button = GTK_MODEL_BUTTON(gtk_builder_get_object(builder, "about_menu_button"));
+	GtkDialog* about_dialog = GTK_DIALOG(gtk_builder_get_object(builder, "about_dialog"));
 
 	// Create "UserInterface" structure
 	UserInterface ui = {
-		//builder,
 		window,
 		header_bar,
 		main_image,
 		resolve_button,
 		nn_dialog,
+		about_dialog,
 	};
 
 	// Connects signal handlers
 	g_signal_connect(quit_button, "clicked", G_CALLBACK(gtk_main_quit), NULL);
 	g_signal_connect(open_button, "clicked", G_CALLBACK(on_open_button), &ui);
 	g_signal_connect(nn_menu_button, "clicked", G_CALLBACK(on_nn_menu_button), &ui);
-	//g_signal_connect(nn_dialog, "delete-event", G_CALLBACK(gtk_widget_hide_on_delete(GTK_WIDGET(nn_dialog))), NULL);
+	g_signal_connect(about_menu_button, "clicked", G_CALLBACK(on_about_menu_button), &ui);
 
 	gtk_window_set_title(GTK_WINDOW(window), "Sudoku Solver");
 
