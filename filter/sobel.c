d600a91cf7903a1ef41106faa5e95baa5e830925
tristan.faure <tristan.faure@epita.fr>
2022-12-07 22:59:49 +0100

    Filte bien fait

--- filter/sobel.c
+++ filter/sobel.c
@@ -92,35 +92,3 @@ SDL_Surface *Sobel(SDL_Surface *image)
 
     return output;
 }
-
-// Loads an image in a surface.
-// The format of the surface is SDL_PIXELFORMAT_RGB888.
-//
-// path: Path of the image.
-SDL_Surface* load_image(const char* path)
-{
-    SDL_Surface* tempSurface = IMG_Load(path);
-    if (tempSurface == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
-
-    SDL_Surface* surface = SDL_ConvertSurfaceFormat(tempSurface, SDL_PIXELFORMAT_RGB888, 0);
-    SDL_FreeSurface(tempSurface);
-    return surface;
-}
-
-
-
-int main(int argc, char** argv)
-{
-    // Checks the number of arguments.
-    if (argc != 3)
-        errx(EXIT_FAILURE, "Usage: imageIN imageOUT");
-    
-    SDL_Surface* surface = Sobel(load_image(argv[1]));
-
-
-    IMG_SaveJPG(surface, argv[2], 100);
-    SDL_FreeSurface(surface);
-
-    return EXIT_SUCCESS;
-}
