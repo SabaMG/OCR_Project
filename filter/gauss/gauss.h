26f1ec350ae8de2af8d7e508718ee60e103e31f0
tristan.faure <tristan.faure@epita.fr>
2022-12-09 22:32:42 +0100

    flou de gauss

---
+++ filter/gauss/gauss.h
@@ -0,0 +1,3 @@
+#pragma once
+
+SDL_Surface* blur(SDL_Surface* imageSurface, int blur_extent);
---
+++ filter/gauss/main.c
@@ -0,0 +1,30 @@
+#include <err.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include "gauss.h"
+
+SDL_Surface* load_image(const char* path)
+{
+        SDL_Surface* tempSurface = IMG_Load(path);
+        if (tempSurface == NULL)
+                errx(EXIT_FAILURE, "%s", SDL_GetError());
+
+        SDL_Surface* surface = SDL_ConvertSurfaceFormat(tempSurface, SDL_PIXELFORMAT_RGB888, 0);
+        SDL_FreeSurface(tempSurface);
+        return surface;
+}
+
+
+int main(int argc, char** argv)
+{
+	if (argc != 3)
+                errx(EXIT_FAILURE, "Usage: imageIN imageOUT");
+
+        SDL_Surface* surface = blur(load_image(argv[1]), 3);
+
+        IMG_SaveJPG(surface, argv[2], 100);
+        SDL_FreeSurface(surface);
+
+        return EXIT_SUCCESS;
+}
