453b27ef749aee6c1e87535ef605a3a94f5a9761
lucas siauve <lucas.siauve@laposte.net>
2022-12-10 18:35:10 +0100

    add rotate letter

--- generate_number/generate_number.py
+++ generate_number/generate_number.py
@@ -6,7 +6,7 @@ def make_dir(number):
     for i in range(number):
         os.mkdir(str(i + 1))
 
-def gen_img(width, height, message, size, font, filename):
+def gen_img(width, height, message, size, angle, font, filename):
     img = Image.new('RGB', (width, height), color='white')
     imgDraw = ImageDraw.Draw(img)
     textWidth = imgDraw.textlength(message, font)
@@ -18,6 +18,7 @@ def gen_img(width, height, message, size, font, filename):
     if(message == "0"):
         message = ""
     imgDraw.text((xText, yText), message, font=font, fill=(0, 0, 0))
+    img = img.rotate(angle, fillcolor='white')
     img.save(filename)
 
 
@@ -28,7 +29,10 @@ path = '/home/lucassiauve/Documents/EPITA/Projet_S3_OCR/generate_number/fonts/'
 fonts = os.listdir(path)
 
 for i in range(10):
-    for font_name in fonts:
-        path_f = path + font_name
-        f = ImageFont.truetype(path_f, 20)
-        gen_img(width, height, str(i), 20, f, 'data/' + str(i) + '/' + font_name + '.jpg')
+    for j in range(15, 30):
+        for k in range (-10, 10):
+            for font_name in fonts:
+                name = font_name[:-4] + "_" + str(j) + "_" + str(k)
+                path_f = path + font_name
+                f = ImageFont.truetype(path_f, 20)
+                gen_img(width, height, str(i), j, k, f, 'data/' + str(i) + '/' + name + '.jpg')
