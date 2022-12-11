d82ba80e6af5c303e757823fc2ce78a8cb4ac9c3
lucas siauve <lucas.siauve@laposte.net>
2022-12-11 16:38:42 +0100

    add

--- generate_number/generate_number.py
+++ generate_number/generate_number.py
@@ -1,20 +1,21 @@
 #Import required library
 from PIL import Image, ImageDraw, ImageFont
 import os
+from random import randint
 
 def make_dir(number):
     for i in range(number):
         os.mkdir(str(i))
 
-def gen_img(width, height, message, size, angle, font, filename):
+def gen_img(width, height, message, l, angle, font, filename):
     img = Image.new('RGB', (width, height), color='white')
     imgDraw = ImageDraw.Draw(img)
     textWidth = imgDraw.textlength(message, font)
     new_box = imgDraw.textbbox((0, 0), message, font)
     new_w = new_box[2] - new_box[0]  # bottom-top
     new_h = new_box[3] - new_box[1]  # right-left]]]]
-    xText = (width - new_w) / 2
-    yText = (height - new_h) / 2 - size / 3
+    xText = (width - new_w) / 2 + l
+    yText = - (height - new_h) / 2 + 5
     if(message == "0"):
         message = ""
     imgDraw.text((xText, yText), message, font=font, fill=(0, 0, 0))
@@ -23,9 +24,15 @@ def gen_img(width, height, message, size, angle, font, filename):
 
 
 def init_dir(main_dir_name):
-    os.mkdir(main_dir_name)
+    if not os.path.exists(main_dir_name):
+        os.mkdir(main_dir_name)
     for i in range(10):
-        os.mkdir(main_dir_name + "/" + str(i))
+        if not os.path.exists(main_dir_name + "/" + str(i)):
+            os.mkdir(main_dir_name + "/" + str(i))
+
+def gray(img, x ,y):
+    r = randint(91, 122);
+    for 
 
 width = 28
 height = 28
@@ -35,12 +42,13 @@ fonts = os.listdir(path)
 
 init_dir("data")
 
-for i in range(10):
-    for j in range(22, 28):
+for i in range(1, 10):
+    for j in range(26, 28):
         for k in range (-10, 10):
-            for font_name in fonts:
-                name = font_name[:-4] + "_" + str(j) + "_" + str(k)
-                path_f = path + font_name
-                f = ImageFont.truetype(path_f, j)
-                gen_img(width, height, str(i), j, k, f, 'data/' + str(i) + '/' + name + '.jpg')
+            for l in range(-3, 3):
+                for font_name in fonts:
+                    name = font_name[:-4] + "_" + str(j) + "_" + str(k)
+                    path_f = path + font_name
+                    f = ImageFont.truetype(path_f, j)
+                    gen_img(width, height, str(i), l, k, f, 'data/' + str(i) + '/' + name + '.jpg')
 
